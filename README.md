39bdab29eacc6d35b7a33992b926689677cb0c8b
priyanshu <priyanshukumari93933@gmail.com>
2025-12-04 19:36:33 +0530

    github profile fetcher

---
+++ README.md
@@ -0,0 +1,9 @@
+# Next.js Assignment Day-1
+
+## Project Title: Responsive Website with Navigation
+
+### Assignment
+Create a responsive website using Next.js and TypeScript that includes a functional navigation bar with four main sections: **Home**, **About**, **Services**, and **Contact**.
+
+---
+
---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Responsive Website with Navigation",
+  description: "Create a responsive website using Next.js and TypeScript that includes a functional navigation",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
